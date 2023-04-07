a964d649b606f9bd87b8daf31b69251a7fea91f6
André Luis <66038323+zezimyy@users.noreply.github.com>
2023-04-07 03:01:22 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@GrownMotionlessTechnicians/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  //Criando a variavel 'a'
+  int a = 0;
+  
+  //Guardando o numero impresso pelo usuario
+  printf("Digite um numero para ser impresso na tela: ");
+  scanf("%d", &a);
+
+  //Printando o valor da variavel
+  printf("%d", a);
+
+}
